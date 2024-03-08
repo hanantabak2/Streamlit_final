3ed92759f385f997767d5d38c99e91dfd48287aa
mayur nawal <mayurnawal007@gmail.com>
2024-03-08 11:27:34 +0400

    adding LLM page

---
+++ pages/7_integrate_LLM.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title(" LLM Compatibility")
+st.image('LLM.png', caption='application can integrate LLM focussed libraries like langchain and can integrate with open source and paid LLM api ')

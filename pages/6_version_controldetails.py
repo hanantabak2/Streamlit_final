5d3bef7da6caadb45064febf322126cbe8d2640a
mayur nawal <mayurnawal007@gmail.com>
2024-03-08 10:41:57 +0400

    adding history page

---
+++ pages/6_version_controldetails.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title(" Code Version - History ")
+st.image('code_version_history.png', caption='within every file you will see the history and you can select display the older version if needed.')

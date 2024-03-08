8f0482c5f68794de748053872c43c389f4878e85
mayur nawal <mayurnawal007@gmail.com>
2024-03-08 10:32:22 +0400

    adding deploy folder

---
+++ pages/5_deploy.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title(" How to deploy the app online")
+st.image('push_files_github.png', caption='Step-1: connect local folder with github and push files there')
+st.image('upload_details_streamlit.png', caption = 'Step-2: make stramlit account, Add details of your private git hub folder on streamlit and deploy')
+
